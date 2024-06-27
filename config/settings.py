fafb5226f053bca659d1f7f68a790434e3323814
Muhammadqodir <155760560+Muhammadqodir17@users.noreply.github.com>
2024-06-27 21:25:02 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -138,6 +138,6 @@ AWS_S3_REGION_NAME = 'us-east-1'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_VERITY = True
-AWS_S3_ADDRESSING_STYLE = 'virtual'
+# AWS_S3_ADDRESSING_STYLE = 'virtual'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STSICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
